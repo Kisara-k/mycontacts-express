faf51a472924d4afbfb2ff36b44da38d17a94183
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-07-04 04:45:20 +0530

    feat: add CRUD endpoints for contacts

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json([
-    // Not necessary, but explicitly setting 200 makes it clear
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@example.com",
-      phone: "123-456-7890",
-    },
-  ]);
-});
+// router.get("/", (req, res) => { ... })
+// This achieves the same functionality, but the following is more useful
+// when you want to handle multiple HTTP methods on the same path
+
+router
+  .route("/")
+
+  .get((req, res) => {
+    res.status(200).json([
+      // Not necessary, but explicitly setting 200 makes it clear
+      {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com",
+        phone: "123-456-7890",
+      },
+    ]);
+  })
+  .post((req, res) => {
+    res.status(201).json({
+      message: "Contact created successfully",
+    });
+  });
+
+router
+  .route("/:id")
+
+  .put((req, res) => {
+    res.status(200).json({
+      message: "Contact updated successfully",
+      id: req.params.id,
+    });
+  })
+  .get((req, res) => {
+    res.status(200).json({
+      message: "Contact retrieved successfully",
+      id: req.params.id,
+    });
+  })
+  .delete((req, res) => {
+    res.status(200).json({
+      message: "Contact deleted successfully",
+      id: req.params.id,
+    });
+  });
 
 module.exports = router;
