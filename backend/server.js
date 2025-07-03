4f2a7460cb01976a2e9d1593a0026dbbc385e681
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-07-04 04:11:22 +0530

    Add environment configuration and update server port handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
