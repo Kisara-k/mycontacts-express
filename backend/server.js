7f468254b66ddf63ea793b079479efd9df0dec8f
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-07-04 04:27:32 +0530

    feat: add GET /api/contacts endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,17 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json([ // Not necessary, but explicitly setting 200 makes it clear
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      phone: "123-456-7890",
+    },
+  ]);
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
